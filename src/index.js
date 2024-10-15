02cb1f28c7e18a23f049ace37b69ccc44b5df45f
Alexandra Spalato <alexaspalato@gmail.com>
2024-10-15 11:48:42 +0200

    expose react

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import VapiBlock from './components/VapiBlock';
 
 // Expose the component globally
 window.VapiBlock = VapiBlock;
 
-// Render the component if the root element exists
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(<VapiBlock />);
+// Expose React, ReactDOM, and VapiBlock globally
+window.React = React;
+window.ReactDOM = ReactDOM;
+window.VapiBlock = VapiBlock;
+
+// Optional: Render for local development
+if (document.getElementById('root')) {
+  ReactDOM.createRoot(document.getElementById('root')).render(<VapiBlock />);
 }
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
