c1f3674f3fb5c4750f0cc3626327912aebf91375
Alexandra Spalato <alexaspalato@gmail.com>
2024-10-15 11:31:40 +0200

    initial commit

---
+++ src/components/VapiBlock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VapiBlock = () => {
+  return (
+    <div className="bg-green-500 p-4 rounded-md text-white uppercase font-bold text-center w-[300px]">
+      VapiBlock
+    </div>
+  );
+};
+
+export default VapiBlock;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import VapiBlock from './components/VapiBlock';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Expose the component globally
+window.VapiBlock = VapiBlock;
+
+// Render the component if the root element exists
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<VapiBlock />);
+}
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
